334a196f9df0d4d1944c67938aa004e125654750
ElliBellii <166251927+ElliBellii@users.noreply.github.com>
2024-09-08 20:11:07 +0200

    Update logs.lua

--- shared/logs.lua
+++ shared/logs.lua
@@ -1,5 +1,5 @@
 Logs = {}
 
 Logs.EnableLogs = true
-Logs.Webhook = 'https://discord.com/api/webhooks/1275793754753863760/OyZdtaf_Ft10q9ft8uNTwa-aIQ5H4lkcsGjBddKbPpioFKpjzYJJ_rUtRc3B2GtGhMoA'
+Logs.Webhook = 'WEBHOOK_HERE'
 Logs.Color = 4162294
